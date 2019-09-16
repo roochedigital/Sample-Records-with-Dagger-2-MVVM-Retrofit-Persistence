411dc09637108d04022e39e717f1a5c52a47688c
r <r>
2019-09-16 09:10:34 +0800

    test dependencies - unit test setup

--- app/src/test/java/com/dearwolves/samplerecords/HomeViewModelTests.java
+++ app/src/test/java/com/dearwolves/samplerecords/HomeViewModelTests.java
@@ -1,7 +1,10 @@
 package com.dearwolves.samplerecords;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class HomeViewModelTests {
 
     @Test
