4d12d17ecbd8eb3bd5ce7625d6b1e7ac00213d28
Jp <jpdynasty75@gmail.com>
2019-11-08 17:56:21 +0800

    Add more test

--- app/src/test/java/com/dearwolves/samplerecords/HomeViewModelTest.kt
+++ app/src/test/java/com/dearwolves/samplerecords/HomeViewModelTest.kt
@@ -5,7 +5,9 @@ import androidx.lifecycle.MutableLiveData
 import com.dearwolves.core.interfaces.IMediaService
 import com.dearwolves.core.interfaces.ISharedPreferenceService
 import com.dearwolves.core.interfaces.IStringService
+import com.dearwolves.core.interfaces.callbacks.RestRequestCallback
 import com.dearwolves.core.model.MediaResponse
+import com.dearwolves.core.model.SearchRequest
 import com.dearwolves.core.repository.LocalRepository
 import com.dearwolves.samplerecords.ui.home.HomeViewModel
 import com.nhaarman.mockitokotlin2.mock
@@ -18,6 +20,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.mockito.MockitoAnnotations
 
+
 @ExperimentalCoroutinesApi
 class HomeViewModelTest {
 
@@ -29,10 +32,17 @@ class HomeViewModelTest {
     private val localRepository: LocalRepository = mock()
     private val sharedPreferenceService: ISharedPreferenceService = mock()
 
+    private val items :ArrayList<MediaResponse> = ArrayList()
 
     @Before
     fun setup() {
         MockitoAnnotations.initMocks(this)
+
+        val item = MediaResponse()
+        item.trackId = 1
+        item.trackName = "Test"
+
+        items.add(item)
     }
 
     @Test
@@ -46,18 +56,41 @@ class HomeViewModelTest {
         assert(list.value!!.size == 1)
     }
 
+    @Test
+    fun testSearchSuccess() {
+        val homeViewModel = createViewModel()
+
+        whenever(
+            mediaService.search(com.nhaarman.mockitokotlin2.any(), com.nhaarman.mockitokotlin2.any())).thenAnswer {
+            (it.arguments[1] as RestRequestCallback<List<MediaResponse>>).onSuccess(items)
+        }
+
+        homeViewModel.search(SearchRequest("star", "au", "movie"))
+
+        verify(localRepository).insertData(items)
+        assert(!homeViewModel.loading.value!!)
+    }
+
+    @Test
+    fun testSearchFailure() {
+        val homeViewModel = createViewModel()
+
+        whenever(
+            mediaService.search(com.nhaarman.mockitokotlin2.any(), com.nhaarman.mockitokotlin2.any())).thenAnswer {
+            (it.arguments[1] as RestRequestCallback<List<MediaResponse>>).onFailure("Error")
+        }
+
+        homeViewModel.search(SearchRequest("star", "au", "movie"))
+
+        assert("Error" == homeViewModel.error.value)
+        assert(!homeViewModel.loading.value!!)
+    }
+
     private fun createViewModel(): HomeViewModel {
-        val item = MediaResponse()
-        item.trackId = 1
-        item.trackName = "Test"
-
-        val list:ArrayList<MediaResponse> = ArrayList()
-        list.add(item)
-
         val data: MutableLiveData<List<MediaResponse>> = MutableLiveData()
-        data.value = list
+        data.value = items
 
-        runBlocking { whenever(localRepository.allData).thenReturn(data) }
+        runBlocking { whenever(localRepository.allData).thenReturn(data)}
         return HomeViewModel(
             mediaService,
             stringService,
