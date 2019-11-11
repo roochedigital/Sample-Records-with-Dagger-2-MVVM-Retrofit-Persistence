be83fe8c14942ef14b656dff263b980520f4f6f8
Jp <jpdynasty75@gmail.com>
2019-11-11 09:29:34 +0800

    Update HomeViewModelTest

--- app/src/test/java/com/dearwolves/samplerecords/HomeViewModelTest.kt
+++ app/src/test/java/com/dearwolves/samplerecords/HomeViewModelTest.kt
@@ -13,7 +13,6 @@ import com.dearwolves.samplerecords.ui.home.HomeViewModel
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Rule
@@ -21,7 +20,6 @@ import org.junit.Test
 import org.mockito.MockitoAnnotations
 
 
-@ExperimentalCoroutinesApi
 class HomeViewModelTest {
 
     @get:Rule
@@ -32,65 +30,117 @@ class HomeViewModelTest {
     private val localRepository: LocalRepository = mock()
     private val sharedPreferenceService: ISharedPreferenceService = mock()
 
-    private val items :ArrayList<MediaResponse> = ArrayList()
+    private val items: ArrayList<MediaResponse> = ArrayList()
+
+    private var date: String = "Sep 17, 9:42 am"
 
     @Before
     fun setup() {
         MockitoAnnotations.initMocks(this)
 
+        // create Sample MediaResponse
         val item = MediaResponse()
         item.trackId = 1
         item.trackName = "Test"
 
+        // add MediaResponse to list
         items.add(item)
     }
 
     @Test
     fun testGetAllData() {
+        // arrange
         val homeViewModel = createViewModel()
 
+        // act
         val list = homeViewModel.getData()
 
+        // assert
+        // verify allData is called
         verify(localRepository).allData
-
+        // expected list size is 1
         assert(list.value!!.size == 1)
     }
 
     @Test
     fun testSearchSuccess() {
+        // arrange
         val homeViewModel = createViewModel()
-
+        val request = SearchRequest("star", "au", "movie")
         whenever(
             mediaService.search(com.nhaarman.mockitokotlin2.any(), com.nhaarman.mockitokotlin2.any())).thenAnswer {
             (it.arguments[1] as RestRequestCallback<List<MediaResponse>>).onSuccess(items)
         }
 
-        homeViewModel.search(SearchRequest("star", "au", "movie"))
+        // act
+        homeViewModel.search(request)
 
+
+        // assert
+        // verify insertData is called
         verify(localRepository).insertData(items)
+        // expected loading value is false
         assert(!homeViewModel.loading.value!!)
     }
 
     @Test
     fun testSearchFailure() {
+        // arrange
         val homeViewModel = createViewModel()
-
+        val request = SearchRequest("star", "au", "movie")
+        val errorMessage = "Error"
         whenever(
             mediaService.search(com.nhaarman.mockitokotlin2.any(), com.nhaarman.mockitokotlin2.any())).thenAnswer {
-            (it.arguments[1] as RestRequestCallback<List<MediaResponse>>).onFailure("Error")
+            (it.arguments[1] as RestRequestCallback<List<MediaResponse>>).onFailure(errorMessage)
         }
 
-        homeViewModel.search(SearchRequest("star", "au", "movie"))
+        // act
+        homeViewModel.search(request)
 
-        assert("Error" == homeViewModel.error.value)
+        // assert
+        // expected error value is "Error"
+        assert(errorMessage == homeViewModel.error.value)
+        // expected loading value is false
         assert(!homeViewModel.loading.value!!)
     }
+
+
+    @Test
+    fun testInit() {
+        // arrange
+        val homeViewModel = createViewModel()
+
+        // act
+        homeViewModel.init()
+
+        //assert
+        // check if observer is set
+        assert(homeViewModel.getData().hasActiveObservers())
+        // check if lastVisit is set
+        assert(date == homeViewModel.lastVisit.value)
+    }
+
+    @Test
+    fun testOnDestroy() {
+        // arrange
+        val homeViewModel = createViewModel()
+
+        // act
+        homeViewModel.init()
+        homeViewModel.onDestroy()
+
+        //assert
+        // check if observer is remove
+        assert(!homeViewModel.getData().hasActiveObservers())
+    }
+
 
     private fun createViewModel(): HomeViewModel {
         val data: MutableLiveData<List<MediaResponse>> = MutableLiveData()
         data.value = items
 
         runBlocking { whenever(localRepository.allData).thenReturn(data)}
+        runBlocking { whenever(sharedPreferenceService.getLastVisit()).thenReturn(date)}
         return HomeViewModel(
             mediaService,
             stringService,
